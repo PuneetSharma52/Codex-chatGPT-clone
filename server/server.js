2d9f5cf977ab5ba2be0fa47e864ee309ee2092d0
Your Name <you@example.com>
2022-12-30 16:55:48 +0530

    Second

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log('AI server started on https://codex-lc6q.onrender.com'))
 
 
 // import express from 'express';
